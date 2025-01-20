a3658bb85e5fc0d9cdf449572f653bee2e193ff3
Christy Haragan <825421+christyharagan@users.noreply.github.com>
2025-01-20 11:25:09 +0000

    Update MyApplication.java

--- app/src/main/java/com/rxjava2/android/samples/MyApplication.java
+++ app/src/main/java/com/rxjava2/android/samples/MyApplication.java
@@ -39,4 +39,13 @@ public class MyApplication extends Application {
                 });
     }
 
+    public void sendAutoEventLonger() {
+        Observable.timer(20, TimeUnit.SECONDS)
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) {
+                        bus.send(new Events.AutoEvent());
+                    }
+                });
+    }
 }
